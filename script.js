f199a694b65ce47fcd91a1530bdac74c34cb04da
AlindaB <alindabyamukama@gmail.com>
2022-03-31 14:51:58 +0300

    JS: completed form validation, form data handling w/ regex, if statements,event-listener, functions validate & storeBookmark

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const modalShow = document.getElementById('show-modal')
 const modalClose = document.getElementById('close-modal')
 const bookmarkForm = document.getElementById('bookmark-form') 
 const websiteNameEl = document.getElementById('website-name')
-const websiteUrlEl = document.getElementById('bookmarks-container')
+const websiteUrlEl = document.getElementById('website-url')
 const bookmarksContainer = document.getElementById('bookmarks-container')
 
 // show modal, focus on input
@@ -12,7 +12,42 @@ function showModal() {
     websiteNameEl.focus()
 }
 
+// validate form
+function validate(nameValue, urlValue) {
+    const expression = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/g
+    const regex = new RegExp(expression)
+    if (!nameValue || !urlValue) {
+        alert('Please submit values for both fields.')
+    }
+    // if (urlValue.match(regex)) {
+    //     alert('match!')
+    // }
+    if (!urlValue.match(regex)) {
+        alert('Please provide a valid website address')
+        return false
+    }
+    // valid
+    return true
+}
+
 // modal event listenenrs
 modalShow.addEventListener('click', showModal)
 modalClose.addEventListener('click', () => modal.classList.remove('show-modal'))
-window.addEventListener('click', (e) => (e.target === modal ? modal.classList.remove('show-modal') : false) )
+window.addEventListener('click', (e) => (e.target === modal ? modal.classList.remove('show-modal') : false))
+
+// handle data from form
+function storeBookmark(e) {
+    e.preventDefault()
+    const nameValue = websiteNameEl.value 
+    let urlValue = websiteUrlEl.value
+    if (!urlValue.includes('http://', 'https://')) {
+        urlValue = `https://${urlValue}`
+    }
+    console.log(nameValue, urlValue)
+    if (!validate(nameValue, urlValue)) {
+        return false
+    }
+}
+
+// event listener
+bookmarkForm.addEventListener('submit', storeBookmark)
